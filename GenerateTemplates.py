b128215166b6d536139f9d6732a2f934c4b3eaa8
Kilfoy64-bit <58482375+Kilfoy64-bit@users.noreply.github.com>
2020-09-27 20:15:41 -0500

    Added experimental cropping function

--- GenerateTemplates.py
+++ GenerateTemplates.py
@@ -1,6 +1,12 @@
 import numpy as np
 from cv2 import cv2
 
+ # initialize the list of reference points and boolean indicating
+# whether cropping is being performed or not
+refPt = []
+cropping = False
+image = None
+
 def readimage(path):
     print(f'readimage({path})')
     with open(path, 'rb') as f:
@@ -151,7 +157,7 @@ def display(video_path):
             cropped_edges = (foreground_morph // 255) * edges_filtered
 
             #EXPERIMENTAl
-            edges_and_foreground = np.add(edges_filtered, foreground)
+            edges_and_foreground = np.add(cropped_edges, foreground)
 
             # Stacking the images to print them together
             # For comparison
@@ -180,11 +186,59 @@ def display(video_path):
     # Closes all the frames
     cv2.destroyAllWindows()
 
+def click_and_crop(event, x, y, flags, param):
+	# grab references to the global variables
+	global refPt, cropping, image
+	# if the left mouse button was clicked, record the starting
+	# (x, y) coordinates and indicate that cropping is being
+	# performed
+	if event == cv2.EVENT_LBUTTONDOWN:
+		refPt = [(x, y)]
+		cropping = True
+	# check to see if the left mouse button was released
+	elif event == cv2.EVENT_LBUTTONUP:
+		# record the ending (x, y) coordinates and indicate that
+		# the cropping operation is finished
+		refPt.append((x, y))
+		cropping = False
+		# draw a rectangle around the region of interest
+		cv2.rectangle(image, refPt[0], refPt[1], (0, 255, 0), 2)
+		cv2.imshow("image", image)
+
+def crop_template():
+    global refPt, cropping
+    image_path = 'test_image.jpeg'
+    image = cv2.imread(image_path)
+    # load the image, clone it, and setup the mouse callback function
+    clone = image.copy()
+    cv2.namedWindow("image")
+    cv2.setMouseCallback("image", click_and_crop)
+    # keep looping until the 'q' key is pressed
+    while True:
+        # display the image and wait for a keypress
+        cv2.imshow("image", image)
+        key = cv2.waitKey(1) & 0xFF
+        # if the 'r' key is pressed, reset the cropping region
+        if key == ord("r"):
+            image = clone.copy()
+        # if the 'c' key is pressed, break from the loop
+        elif key == ord("c"):
+            break
+    # if there are two reference points, then crop the region of interest
+    # from teh image and display it
+    if len(refPt) == 2:
+        roi = clone[refPt[0][1]:refPt[1][1], refPt[0][0]:refPt[1][0]]
+        cv2.imshow("ROI", roi)
+        cv2.waitKey(0)
+    # close all open windows
+    cv2.destroyAllWindows()
+
 def main():
     print('main()')
     # image_display_test('test_image.jpeg')
     # display('./fall_samples/fall-01-cam0.mp4')
     display(None)
+    # crop_template()
     
 if __name__ == '__main__':
     main()
