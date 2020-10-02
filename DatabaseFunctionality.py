2dc6d1b69c218643c4cd7245ba9efb5493ece11b
Kilfoy64-bit <58482375+Kilfoy64-bit@users.noreply.github.com>
2020-10-02 12:43:13 -0500

    Merged file functionality
    GenerateTemplates.py was split into GenerateTempaltes.py and main.py
    functionality was added to import other modules

---
+++ DatabaseFunctionality.py
@@ -0,0 +1,217 @@
+import psycopg2
+
+
+conn = None
+standing_array = []
+falling_array = []
+sitting_array = []
+lying_array = []
+
+
+def connect():
+    global conn
+    # conn parameters
+    dbname = input("Database name: ")
+    pword = input("Password: ")
+    try:
+        # Attempts to connect to server
+        print("Connecting...")
+        conn = psycopg2.connect(host = 'localhost', database = dbname, user = 'postgres', password = pword)
+        print("Connection successful.")
+        print_db_version()     
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+      
+
+def disconnect():
+    # closes communcation with PostgreSQL server
+    global conn
+    print("Disconnecting...")
+    if conn is not None:
+        conn.close()
+    print("Disconnection successful.")
+
+
+def print_db_version():
+    print('PostgreSQL database version:')
+    try:
+        curr = conn.cursor()
+        curr.execute('SELECT version()')
+        db_version = curr.fetchone()
+        curr.close()
+        print(db_version)
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+
+def manage_database_menu():
+    print("manage_database_menu() called")
+    show_manage_menu = True
+    while show_manage_menu:
+        print("""
+        Command:        Description:\n
+        1         ---   Add template.\n
+        2         ---   Delete template.\n
+        3         ---   Modify template.\n
+        4         ---   Access template image by template_id.\n
+        r         ---   Return to previous menu.
+        """)
+        command = input("Enter a command: ")
+        if command == "1":
+            print(f"{command} selected.")
+            image_path = input("Please enter the path of the image: ")
+            template_type = input("Please enter the template_type: ")
+            verify_image_path(image_path)
+            image_byte_array = read_image_to_byte_array(image_path)
+            add_template(template_type, image_path, image_byte_array)
+        elif command == "2":
+            print(f"{command} selected.")
+            delete_template_id = input("Enter the template_id you wish to delete: ")
+            delete_template(delete_template_id)
+        elif command == "3":
+            # TODO: Add functionality to modify template
+            print(f"{command} selected.")
+        elif command == "4":
+            print(f"{command} selected.")
+            template_id = input("Enter template_id: ")
+            access_image_by_id(template_id)
+        elif command == "r":
+            show_manage_menu = False
+        else:
+            print(f"{command} is not a valid command.\nPlease try again.")
+
+
+def verify_image_path(image_path):
+    #TODO: add functionality to verify an image.
+    print("Image verified.")
+
+
+def add_template(template_type, image_name, image_byte_array):
+    #TODO: add functionality to extract image_name from path.
+    #      Maybe find last '/' and have name = what's left.
+    global conn
+    try:
+        curr = conn.cursor()
+        print("adding template...")
+        curr.execute('''
+        INSERT INTO template (template_type, image_name, image)
+        VALUES(%s, %s, %s)''', (template_type, image_name, image_byte_array))
+        print("Updating template table...")
+        curr.execute('''
+        UPDATE template
+        SET image = %s''', (image_byte_array + b'1',))
+        conn.commit()
+        curr.close()
+        print("Added template successfully.")
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    curr.close()
+    
+
+def delete_template(template_id):
+    global conn
+    try:
+        curr = conn.cursor()
+        curr.execute('''
+        DELETE FROM template
+        WHERE template_id = %s
+        ''', (template_id))
+        conn.commit()
+        curr.close()
+        print("Deleted template successfully.")
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+
+def access_image_by_id(template_id):
+    global conn
+    try:
+        curr = conn.cursor()
+        curr.execute('''
+        SELECT image
+        FROM template
+        WHERE template_id = %s
+        ''', (template_id,))
+        mview = curr.fetchone()
+        image = mview[0].tobytes()
+        print("First 10 bytes: " + str(image[:10]))
+        curr.close()
+        return image
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+
+def access_all_image_by_type(template_type):
+    global conn
+    try:
+        curr = conn.cursor()
+        curr.execute('''
+        SELECT image
+        FROM template
+        WHERE template_type = %s
+        ''', (template_type,))
+        rows = curr.fetchall()
+        template_type_array = []
+        for row in rows:
+            template_type_array.append(row[0].tobytes())
+        curr.close()
+        print(f"Successfully loaded {template_type} array.")
+        return template_type_array
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+
+def quit_application():
+    print("quit_application() called")
+    disconnect()
+
+
+def read_image_to_byte_array(image_path):
+    with open(image_path, 'rb') as f:
+        b = bytearray(f.read())
+        return b
+
+
+def load_template_type_arrays():
+    global standing_array
+    global falling_array
+    global sitting_array
+    global lying_array
+
+    standing_array = access_all_image_by_type("standing")
+    falling_array = access_all_image_by_type("falling")
+    sitting_array = access_all_image_by_type("sitting")
+    lying_array = access_all_image_by_type("lying")
+
+
+def main():
+    global conn
+    application_running = False
+    connect()
+    if conn is not None: 
+        application_running = True
+    
+    while application_running:
+        print("""
+        Command:                   Description:\n
+        Manage Database(m)       --   Add, Delete, or Access information stored in Database.\n
+        Load template arrays(l)  --   Loads template_type arrays (This will be automatic in future builds).\n
+        Quit(q)                  --   Terminates program and disconnects from postgreSQL server.
+        """)
+
+        user_command = input("Enter a command: ")
+        if user_command == "m":
+            manage_database_menu()
+        elif user_command == "q":
+            application_running = False
+        elif user_command == "l":
+            load_template_type_arrays()
+        else:
+            print("\nINCORRECT COMMAND ENTERED")
+    
+    quit_application()
+
+
+if __name__ == '__main__':
+    main()
+    print("FINISHED")
---
+++ main.py
@@ -0,0 +1,193 @@
+import numpy as np
+from cv2 import cv2
+
+# Our moduels
+import GenerateTemplates
+import DatabaseFunctionality
+
+refPt = []
+cropping = False
+current_frame = None
+
+def readimage(path):
+    print(f'readimage({path})')
+    with open(path, 'rb') as f:
+        return f.read()
+
+def byteStr_to_image(source_str):
+    print('byteStr_to_image')
+    decoded = cv2.imdecode(np.frombuffer(source_str, np.uint8), -1)
+    return decoded
+
+def show_image(source):
+    print('show_image()')
+    print(type(source))
+    cv2.imshow('Image', source)
+    while True:
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+    return cv2.destroyAllWindows()
+
+# displays image from file
+def image_display_test(image_path):
+    print('image_display_test()')
+    image_bytes = readimage(image_path)
+    img = byteStr_to_image(image_bytes)
+    print('OpenCV:\n', img)
+    show_image(img)
+
+def display(video_path = None):
+    file_count = 0
+    
+    if video_path is not None:
+        cap = cv2.VideoCapture(video_path)
+    else:
+        cap = cv2.VideoCapture(cv2.CAP_DSHOW)
+    
+    # Check if camera opened successfully
+    if (cap.isOpened()== False): 
+        print("Error opening video stream or file")
+
+    fgbg = cv2.createBackgroundSubtractorMOG2(
+        history=10,
+        varThreshold=2,
+        detectShadows=False)
+
+    # Read the video
+    while(cap.isOpened()):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        if ret == True:
+        
+            # Converting the image to grayscale.
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            # Smoothing without removing edges.
+            gray_filtered = cv2.bilateralFilter(gray, 7, 75, 75)
+            
+            # Extract the foreground
+            foreground = fgbg.apply(gray_filtered)
+            
+            # Smooth out to get the moving area
+            kernel_close = np.ones((10,10),np.uint8)
+            kernel_open = np.ones((10,10),np.uint8)
+            kernel_dilate = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2,2))
+
+            foreground_morph_close = cv2.morphologyEx(foreground, cv2.MORPH_CLOSE, kernel_close)
+            foreground_morph_open = cv2.morphologyEx(foreground_morph_close, cv2.MORPH_OPEN, kernel_open)
+            foreground_morph_close = cv2.morphologyEx(foreground_morph_open, cv2.MORPH_CLOSE, kernel_close)
+            foreground_morph_dilate = cv2.dilate(foreground_morph_close,kernel_dilate,iterations = 1)
+
+            edges_filtered = cv2.Canny(gray_filtered, 60, 120)
+
+            # Crop off the edges out of the moving area
+            cropped_edges = (foreground_morph_dilate // 255) * edges_filtered
+
+            #EXPERIMENTAl
+            layered_frames = np.add(cropped_edges, foreground_morph_dilate)
+
+            # Stacking the images to print them together
+            # For comparison
+            frames_normal = np.hstack(( gray,  gray_filtered))
+            frames_edges = np.hstack((edges_filtered,  cropped_edges))
+            foreground_morphs = np.hstack((foreground_morph_close, foreground_morph_open))
+
+            # Display the resulting frame
+            cv2.imshow('Normal Frames', frames_normal)
+            cv2.imshow('Frames Edges', frames_edges)
+            cv2.imshow('Foreground Frames', foreground)
+            cv2.imshow('foreground_morphs', foreground_morphs)
+            cv2.imshow('layered Frames', layered_frames)
+
+            cv2.imwrite('./templates/layered_frames/test_template' + str(file_count) + '.png', layered_frames)
+            file_count += 1
+
+            # Press Q on keyboard to  exit
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+        
+        # Break the loop
+        else: 
+            break
+    
+    # When everything done, release the video capture object
+    cap.release()
+    
+    # Closes all the frames
+    cv2.destroyAllWindows()
+
+def compare_template_to_frame(template_path, frame_path):
+    print('compare_template_to_frame()')
+    template = cv2.imread(template_path)
+    frame = cv2.imread(frame_path)
+    print(template.shape)
+    row_difference = frame.shape[0] - template.shape[0] 
+    column_difference = frame.shape[1] - template.shape[1]  
+    n = 0
+    highest_similarity = 0.0
+    for starting_ypoint in range(0, row_difference + 1 ,template.shape[1]//4):
+        for starting_xpoint in range(0, column_difference + 1,template.shape[1]//4):
+            n += 1
+            temp = image_compare(frame, template, (starting_ypoint, starting_xpoint))
+            if temp > highest_similarity:
+                highest_similarity = temp
+    print(f'n: {n} comparisons')
+    print(f'highest_similarity_percent: {highest_similarity}')
+    
+def image_compare(source, comparison, starting_point):
+    # print('image_compare()')
+    common_pixels = 0
+    total_pixels_compared = comparison.shape[0] * comparison.shape[1]
+    similarity_percent = 0.0
+    
+    for y in range(starting_point[0], starting_point[0] + comparison.shape[0], 1):
+        for x in range(starting_point[1], starting_point[1]+ comparison.shape[1], 1):
+            
+            comp_x = x - starting_point[1]
+            comp_y = y - starting_point[0]
+            if (source[y][x] == comparison[comp_y][comp_x]).all():
+                common_pixels += 1
+            
+    similarity_percent = common_pixels/total_pixels_compared * 100
+    
+    return similarity_percent
+
+def User_interface():
+    
+    while True:
+        print("""
+        Command:(button)              Description:
+        view_video:(1)                Displays available videos in 'fall_samples' with computer vision.
+        view_webcam:(2)               Displays connected webcam with computer vision
+        crop_templates:(3)            Allows user to crop templates that exist in 'templates/layered_frames'.
+        compare_template:(4)          Demonstrates comparing a template to a frame.
+        quit:(q)
+        """)
+        command = input("Command: ")
+        if command == '1':
+            available_videos = ['./fall_samples/fall-01-cam0.mp4', './fall_samples/fall-27-cam0.mp4']
+            print("Please choose from: ")
+            i = 0
+            for video in available_videos:
+                print(str(i) + video)
+                i+=1
+            selection = int(input("Enter: "))
+            display(available_videos[selection])
+
+        elif command == '2':
+            display()
+        elif command == '3':
+            GenerateTemplates.crop_template()
+        elif command == '4':
+            compare_template_to_frame('./templates/cropped_templates/falling69.png', './templates/layered_frames/test_template69.png')
+        elif command == 'q':
+            break
+        else:
+            print("incorrect command.")
+def main():
+    print('main()')
+    User_interface()
+
+if __name__ == '__main__':
+    main()
+    
